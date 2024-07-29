8769741e39a248824e54b0557671961d9fb5dce7
jamiu23 <salawujamiuolatunji60@gmail.com>
2024-07-29 13:26:12 +0100

    updated

--- function.js
+++ function.js
@@ -12,3 +12,8 @@ let inbox = document.getElementById("usersMessages")
 inbox.onclick = function(){
     $('#inboxModal').modal ('show');
 }
+
+let chat = document.getElementById("inboxUsersMessages")
+chat.onclick = function () {
+    $('#chatModal').modal ('show');
+}

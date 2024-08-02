4f0e0f101bfe6c0cda6164e6a3f714b06a0fbb80
jamiu23 <salawujamiuolatunji60@gmail.com>
2024-08-02 16:18:36 +0100

    updated

--- function.js
+++ function.js
@@ -1,29 +1,56 @@
 // X
+// show signin modal onload
 function showModal() {
     $('#modal').modal('show');
-}
-
-let messages = document.getElementById("messages")
-messages.onclick = function(){
-    $('#messagesModal').modal ('show');
-}
-
-let inbox = document.getElementById("usersMessages")
-inbox.onclick = function(){
-    $('#inboxModal').modal ('show');
-}
-
-let chat = document.getElementById("inboxUsersMessages")
-chat.onclick = function () {
-    $('#chatModal').modal ('show');
-}
-
+};
 
 // load homepage
 function redirectToNewPage() {
     setTimeout(() => {
         window.location.href = "homepage.html";
     }, 5000); 
+};
+
+// message side
+// div hoolding messages and angle icon show inbox modal onclick
+let message = document.getElementById("message")
+message.onclick = function(){
+    $('#inboxModal').modal ('show');
+};
+
+// message icon onclick show center modal
+function newMessage(){
+    $('#messagesModal').modal ('show');
+};
+
+// angle up onclick shows inbox
+function newMessageOpen(){
+    $('#inboxModal').modal ('show'); 
+};
+
+
+// inbox side
+function messageClosed(){
+    $('#inboxModal').modal ('hide');
+};
+
+function showChat(){
+    $('#chatModal').modal ('show');  
+};
+
+function closeInbox(){
+    $('#inboxModal').modal ('hide');  
+};
+
+function undoChat() {
+    $('#inboxModal').modal ('show');
+};
+
+
+function closeChat(){
+    $('#chatModal').modal ('hide');
+};
+
+function directChat(){
+    $('#chatModal').modal ('show');
 }
-
-

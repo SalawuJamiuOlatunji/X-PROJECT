2b1677b222e14ce079fb32cb9e11b3ccfc0be331
jamiu23 <salawujamiuolatunji60@gmail.com>
2024-08-02 17:08:30 +0100

    updated

--- function.js
+++ function.js
@@ -18,50 +18,51 @@ message.onclick = function(){
     $('#inboxModal').modal ('show');
 };
 
-// message icon onclick show center modal
+// message icon onclick show messagesModal
 function newMessage(){
     $('#messagesModal').modal ('show');
 };
-
+// onclick hide messagesModal and show chatModal
 function nextChat(){
     $('#messagesModal').modal ('hide');
     $('#chatModal').modal ('show');
 };
-
+// onclick change nextButton backgroundColor and color
 let nextButtonColor = document.getElementById("nextButtonColor")
 function nextButton(){
     nextButtonColor.style.backgroundColor = "white";
     nextButtonColor.style.color = "black";
 };
 
-// angle up onclick shows inbox
+// angle up onclick shows inboxModal
 function newMessageOpen(){
     $('#inboxModal').modal ('show'); 
 };
 
 
 // inbox side
+// onclick hide inboxModal
 function messageClosed(){
     $('#inboxModal').modal ('hide');
 };
-
+// onclick show chatModal
 function showChat(){
     $('#chatModal').modal ('show');  
 };
-
+// onclick hide inboxModal
 function closeInbox(){
     $('#inboxModal').modal ('hide');  
 };
-
+// onclick show inboxModal
 function undoChat() {
     $('#inboxModal').modal ('show');
 };
 
-
+// onclick hide chatModal
 function closeChat(){
     $('#chatModal').modal ('hide');
 };
-
+// onclick show chatModal
 function directChat(){
     $('#chatModal').modal ('show');
 }
